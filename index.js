3bb5130c41465f42ad39fbb5426d34f626670da5
Sphinx <71399083+vrzc@users.noreply.github.com>
2024-01-09 20:14:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js-selfbot-v13") // npm i discord.js-selfbot-v1
 const client = new Discord.Client({checkUpdate: false})
 const sphinx = require("sphinx-self");
 
-new sphinx.Core(client).leveling({ channel: "Channel ID" }) //hover for options
+new sphinx.Core(client).leveling({ channel: "1066775934960676994" }) //hover for options
 
-client.login("Your Discord Token") //Not saved.
+client.login(process.env.TOKEN) //Not saved.
