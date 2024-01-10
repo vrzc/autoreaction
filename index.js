afbc5fb384125b1c2158f32f0270b4e808db1974
Sphinx <71399083+vrzc@users.noreply.github.com>
2024-01-10 17:56:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js-selfbot-v13") // npm i discord.js-selfbot-v13 (required)
 const client = new Discord.Client({checkUpdate: false})
 const sphinx = require("sphinx-self");
+const keep_alive = require("./keep_alive.js");
 
 new sphinx.Core(client).leveling({ channel: "1066775934960676994", randomLetters: true, type: 'ar', time: 3000 }) //hover for options
 
